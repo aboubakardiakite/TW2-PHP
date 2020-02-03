aaf6b6d5e66b65bc8628db8a1f90409fbe826aa7
Diakite Aboubakar-siriki <diakite@a10p21.fil.univ-lille1.fr>
2020-02-03 20:32:43 +0100

    tout

--- ASD/tp-experimentateur/src/test.py
+++ ASD/tp-experimentateur/src/test.py
@@ -56,34 +56,37 @@ def recherche_dichotomique( element, liste_triee):
     a = 0
     b = len(liste_triee)-1
     m = (a+b)//2
-    while a < b :
+    while a < b:
         if str(liste_triee[m])==str(element):
             return m
         elif str(liste_triee[m]) > str(element) :
             b = m-1
-        else :
+        else:
             a = m+1
         m = (a+b)//2
     return a
 
 def negative_markers2(markers,positive):
     negative = np.array([])
-    i=0
-    s.merge_sort(positive,compare)
-    while(i<len(markers)):
-        j=0
-        while(j<len(positive)):
-            indice = recherche_dichotomique( markers[i], positive)
-            if(compare(markers[i],positive[indice])):
-                t=False
-                break
-            else:
-                t=True
-            j=j+1
-        if(t==True):
+    m=len(markers)
+    p=len(positive)
+    positive=s.merge_sort(positive,compare)
+    t=1;
+    for i in range(0,m):
+        j=0;
+        
+        indice = recherche_dichotomique( markers[i], positive)
+        if(compare(markers[i],positive[indice])==0):
+            t=1
+            
+        else:
+            t=0
+        
+        if(t==0):
             negative=np.append(negative,markers[i])       
-        i=i+1
+        
     return negative
+
 
 # STRATEGY 3
 def negative_markers3(markers,positive):
@@ -91,18 +94,18 @@ def negative_markers3(markers,positive):
 
     markers=s.merge_sort(markers,compare)
     positive=s.merge_sort(positive,compare)
-
-    while(len(markers)>0 and len(positive)>0):
-        if(compare(markers[0],positive[0])==0):
-            markers=markers[1:len(markers)]
-            positive=positive[1:len(positive)]
-        else:
-            negative=np.append(negative,markers[0])
-            markers=markers[1:len(markers)]
-        
-
-    if(len(markers)!=0):
-        negative=np.append(negative,markers)
+    i=0
+    j=0
+    while(j<len(positive)):
+        comp=compare(markers[i],positive[j])
+        if(comp==0):
+            i=i+1;
+            j=j+1;
+        elif(comp<0):
+            negative=np.append(negative,markers[i])
+            i=i+1
+    for k in range(10,len(markers)):
+        negative=np.append(negative,markers[k])
     
     return negative
     
@@ -118,7 +121,9 @@ if __name__ == "__main__":
     exp = experience.Experience(p,m)
     markers = exp.get_markers()
     positive = exp.get_positive_markers()
-
+    
+    
+"""
     print("Markers: %s" % (markers))
     print("Positive markers: %s" % (positive))
     
@@ -136,4 +141,4 @@ if __name__ == "__main__":
     cpt = 0
     print("Negative markers: %s" % (negative_markers3(markers,positive)))
     print("Nb. comparisons: %d" % (cpt))
-
+"""
