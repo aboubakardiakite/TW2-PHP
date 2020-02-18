e7c752339d6ddd822732d76fbf879ab6e0b6919b
diakite <aboubakarsiriki.diakite.etu@univ-lille.fr>
2020-02-18 01:26:12 +0100

    re

--- PROBA/TP1_Diakite_aboubakar_siriki/commande.R
+++ PROBA/TP1_Diakite_aboubakar_siriki/commande.R
@@ -24,12 +24,14 @@ M=matrix(data=AirPassengers, nrow=12,ncol=12, byrow= TRUE)#byrow qui permet de b
 AirPassengers
 n=M[10:11,2:6]#extraire les valeur en commencant par les ncol
 m=data.frame(row.names=c("Jean","Fatima","kevin","Eduardo"),taille=c(168,175,172,183),poids=c(67,75,69,81))
+
+
 help(morley)
 example(morley)
 help(mean)
 exp=morley[,3]#affecte a exp la vitesse des 5 expercice
 vitesse1=exp[0:20]#affecte les vitesse de la premiere experience
-vitesse2=exp[21:40]#affecte les vitesse de la deuxieme experience
+vitesse2=exp[21:40]#affecte les vitesse de la deuxie  me experience
 vitesse3=exp[41:60]#affecte les vitesse de la troisieme experience
 vitesse4=exp[61:80]#affecte les vitesse de la quatrieme experience
 vitesse5=exp[81:100]#affecte les vitesse de la cinquieme experience
@@ -64,3 +66,30 @@ hist(vitesse3)
 hist(vitesse4)
 hist(vitesse5)
 hist(vitesseTotal)
+
+M=matrix(data=AirPassengers, nrow=12,ncol=12, byrow= TRUE)#byrow qui permet de bien remplie la matrix colone et ligne respecter
+M1=M[8:12,1:12]#recuperrer les valeurs des années 1956-1960
+hist(M1)#on obseve 8 classes
+hist(M1,breaks = 5)#grace à break il fera 5 classes,on constate que les valeur comprise entre 300 et 400 viennent tres frequement
+
+t=seq(0,2*pi,0.05)#je crée un interval donc [0,2pi]
+x=sin(t)#creation du vecteur x
+plot(t,x)#tracer de la courbe representant sin(t)
+hist(x)#on constate que les valeur -1 et 1 apparessent plus frequenment tant disque les aute sont constante
+hist(x,nclass = 4)#avec 4 classes c est plus precis et justifie mon explication precedent
+
+  help("curve")#voir l'aide concernant curve
+curve(exp(-x^2/2)/sqrt(2*pi),-2,2)
+
+mu=0
+o=1;
+curve(exp(-(x-mu)^2/2*o^2)/o*sqrt(2*pi),-4,4,add=TRUE,col="red")#je fais un curve de -4 a 4
+
+mu=1
+o=0.2;
+curve(exp(-(x-mu)^2/2*o^2)/o*sqrt(2*pi),-4,4,add=TRUE,col="black")#je fais un curve de -4 a 4
+
+mu=2
+o=0.6;
+curve(exp(-(x-mu)^2/2*o^2)/o*sqrt(2*pi),-4,4,add=TRUE,col="blue")#je fais un curve de -4 a 4
+help(dens)
--- Tw2/Diakité_aboubakar_TP4/fichiersDistribués/figures.php
+++ Tw2/Diakité_aboubakar_TP4/fichiersDistribués/figures.php
@@ -1,9 +1,19 @@
 <?php
- require('lib/fonctionsSVG.php');
+require('lib/fonctionsSVG.php');
  $centreX= 70;
- $centreY= 90;
- $rayon = 50;
- $angle=0;
- /* inclusion de la page */
- require('views/pageFigures.php');
+ $centreY=50;
+ $rayon =52 ;
+ $angle=sc;
+
+ if(!isset($angle) || $angle=='') $angle=50;
+
+ if(isset($centreX,$centreY,$rayon) and is_numeric($angle) and is_numeric($centreY) and is_numeric($centreX) and is_numeric($rayon)) {
+	 
+	 /* inclusion de la page */
+	 require('views/pageFigures.php');
+ }else{
+	
+		 /* inclusion de la page d'erreur */
+	 require('views/pageErreur.html');
+}
 ?>
